187e2cf8a505b730dae12466cfe2527abcb087bf
seanreid1997 <reid.sean14@yahoo.com>
2022-04-19 19:48:12 +0000

    Add questions to questions.js

---
+++ assets/javascript/questions.js
@@ -0,0 +1,16 @@
+let questions = [{
+    question: 'What is 12 x 12?',
+    answer1: '234',
+    answer2: '122',
+    answer3: '111',
+    answer4: '144',
+    correctAnswer: 4
+},
+{
+    question: 'How many 100ml bottles of water can fit in a 1litre container?',
+    answer1: '2',
+    answer2: '10',
+    answer3: '4',
+    answer4: '8',
+    correctAnswer: 2
+}]
