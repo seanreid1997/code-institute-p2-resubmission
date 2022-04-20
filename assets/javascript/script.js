78c92a8fd1d126a71f11504cb6d1d3e16a0e2057
seanreid1997 <reid.sean14@yahoo.com>
2022-04-20 22:07:22 +0000

    Add empty functions to script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,3 +2,22 @@ const questions = document.getElementById('questions');
 const answers = document.getElementsByClassName('answers');
 const progressBar = document.getElementById('progressBar');
 const score = document.getElementById('score');
+
+/**
+ * Function that starts the game by
+ * calling all relevant functions.
+ */
+function startGame(){
+}
+
+/**
+ * Function that generates new random questions
+ */
+function getNewQuestion(){
+}
+
+/**
+ * Function that increments the users score
+ */
+function incrementScore(){
+}
