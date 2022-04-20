c824baadc702ce397da34ebad85766ad29ab9251
seanreid1997 <reid.sean14@yahoo.com>
2022-04-20 21:58:21 +0000

    Add variables to script.js for all game divs in game.html

---
+++ assets/javascript/script.js
@@ -0,0 +1,4 @@
+const questions = document.getElementById('questions');
+const answers = document.getElementsByClassName('answers');
+const progressBar = document.getElementById('progressBar');
+const score = document.getElementById('score');
