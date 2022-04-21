dfb2e73473d5b6936e375da11abc7f0f20a6075c
seanreid1997 <reid.sean14@yahoo.com>
2022-04-21 21:06:20 +0000

    Add basic code to functions in script.js.Add questions to script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,46 @@
+let questions = [{
+    question: 'What is 12 x 12?',
+    answer1: '234',
+    answer2: '122',
+    answer3: '111',
+    answer4: '144',
+    correctAnswer: 4
+},
+{
+    question: 'How many 100ml bottles of water can fit in a 1litre container?',
+    answer1: '2',
+    answer2: '10',
+    answer3: '4',
+    answer4: '8',
+    correctAnswer: 2
+}]
+
 const question = document.getElementById('question');
 const answers = document.getElementsByClassName('answers');
 const progressBar = document.getElementById('progressBar');
 const score = document.getElementById('score');
+const MAX_QUESTIONS = 2;
+
+let questionCounter;
+let userScore;
 
 /**
  * Function that starts the game by
  * calling all relevant functions.
  */
 function startGame(){
+    questionCounter = 0;
+    userScore = 0;
+    availableQuestions = getNewQuestion(questions, MAX_QUESTIONS);
+    console.log(availableQuestions)
 }
 
 /**
  * Function that generates new random questions
  */
 function getNewQuestion(){
+    availableQuestions = Math.floor(Math.random() * questions.length)
+
 }
 
 /**
@@ -21,3 +48,5 @@ function getNewQuestion(){
  */
 function incrementScore(){
 }
+
+startGame();
