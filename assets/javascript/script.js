9c80b11009a8747fb3331af7a222ab85c427a4b6
seanreid1997 <reid.sean14@yahoo.com>
2022-04-26 18:06:40 +0000

    Remove unneccessary link from all pages

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,28 @@ console.log('CONNECTED')
 var element = document.getElementsByClassName('container');
 var start = document.getElementById('start');
 
+let questions = [{
+    question: 'hi',
+    answer: '1',
+    answer: '2'
+},
+{
+    question: 'hello',
+    answer: '1',
+    answer: '2'
+},
+{
+    question: 'hello',
+    answer: '1',
+    answer: '2'
+}
+]
+
+for (question in questions){
+    console.log(question);
+}
+
+
 start.addEventListener('click', myFunction)
 
 function myFunction() {
