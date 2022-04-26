6ed50d668f37ff8218a52203e0787454a5b6f1c0
seanreid1997 <reid.sean14@yahoo.com>
2022-04-26 17:58:25 +0000

    Add start button to game page and restart javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,39 +1,10 @@
-let questions = [{
-    question: 'What is 12 x 12?',
-    answer1: '234',
-    answer2: '122',
-    answer3: '111',
-    answer4: '144',
-    answer: 4
-},
-{
-    question: 'How many 100ml bottles of water can fit in a 1litre container?',
-    answer1: '2',
-    answer2: '10',
-    answer3: '4',
-    answer4: '8',
-    answer: 2
-}]
+console.log('CONNECTED')
+var element = document.getElementsByClassName('container');
+var start = document.getElementById('start');
 
-const question = document.getElementById('question');
-const answers = document.getElementsByClassName('answers');
-const progressBar = document.getElementById('progressBar');
-const score = document.getElementById('score');
-const MAX_QUESTIONS = 2;
-const answer = questions[answer]
+start.addEventListener('click', myFunction)
 
-console.log(questions);
-let questionCounter;
-let userScore;
-let acceptingAnswer = false;
-
-/**
- * Function that starts the game by
- * calling all relevant functions.
- */
-function startGame(){
-    questionCounter = 0;
-    userScore = 0;
-    availableQuestions = getRandomQuestion(questions, MAX_QUESTIONS);
+function myFunction() {
+    start.classList.add('hide');
+    element.classList.remove('hide');
 }
-
