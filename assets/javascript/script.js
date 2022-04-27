5d39b89082c1a2a29097917e24d90559f727088a
seanreid1997 <reid.sean14@yahoo.com>
2022-04-27 09:26:28 +0000

    Add empty functions for questions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,24 @@
-let quse
+let questions = [{
+    question: 'What is 12 x 12?',
+    answer1: '234',
+    answer2: '122',
+    answer3: '111',
+    answer4: '144',
+    correctAnswer: 4
+},
+{
+    question: 'How many 100ml bottles of water can fit in a 1litre container?',
+    answer1: '2',
+    answer2: '10',
+    answer3: '4',
+    answer4: '8',
+    correctAnswer: 2
+}]
 
 console.log('CONNECTED')
 const element = document.getElementById('game-container');
 const start = document.getElementById('start');
+let answers = document.getElementsByClassName('answers')
 
 
 /**
@@ -12,7 +28,14 @@ const start = document.getElementById('start');
 function startGame(){
     
     start.classList.add('hide');
+    element.classList.remove('hide');
+    return element
+}
 
-    hide = element.classList.remove('hide');
-    return hide
+function newQuestion() {
+    
+}
+
+function randomQuestion() {
+    
 }
