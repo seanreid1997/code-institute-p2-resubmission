bc89404085ed1ea3e042e65dbd6793da20aa2a08
seanreid1997 <reid.sean14@yahoo.com>
2022-04-30 21:59:57 +0000

    Test code for javascript

---
+++ assets/javascript/questions.js
@@ -0,0 +1,19 @@
+const questions = [{
+    question: 'What is 242 + 25?',
+    choices: [{
+        choice1: '269',
+        choice2: '267',
+        choice3: '217',
+        choice4: '278',
+        answer: 2
+}]},
+    {
+        question: "What is 150 / 5?",
+        choices: [{
+        choice1: "30",
+        choice2: "25",
+        choice3: "3",
+        choice4: "20",
+        answer: 1,
+    }]},
+]
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,25 +1,9 @@
 console.log('CONNECTED')
-const questions = [{
-    question: 'What is 12 x 12?',
-    answer1: '234',
-    answer2: '122',
-    answer3: '111',
-    answer4: '144',
-    correctAnswer: 4
-},
-{
-    question: 'How many 100ml bottles of water can fit in a 1litre container?',
-    answer1: '2',
-    answer2: '10',
-    answer3: '4',
-    answer4: '8',
-    correctAnswer: 2
-}]
 
 const element = document.getElementById('game-container');
 const start = document.getElementById('start');
 const aquestion = document.getElementById('question')
-const answers = document.getElementsByClassName('answers')
+const answers = document.getElementById('answers')
 var randQuestion;
 var currentQuestionIndex;
 
@@ -33,18 +17,18 @@ function startGame(){
     start.classList.add('hide');
     randQuestion = questions.sort(() => Math.random() - 5)
     currentQuestionIndex = 0;
-
     element.classList.remove('hide');
-    return element
     newQuestion()
 }
 
 function newQuestion() {
-    showQuestion(randQuestion[currentQuestionIndex]);
+  showQuestion(randQuestion[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
-    aquestion.innerText = 'hello';
-    return aquestion;
+  aquestion.innerText = question.question
+  answers.innerText = question.choices[0]
+  console.log()
+
 }
 
