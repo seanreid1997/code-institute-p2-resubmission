49b43bff2bf51284447a01a877a12adc81fd22ae
seanreid1997 <reid.sean14@yahoo.com>
2022-05-01 11:01:06 +0000

    Add choices to answers innertext of the main game

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,9 +4,13 @@ const element = document.getElementById('game-container');
 const start = document.getElementById('start');
 const aquestion = document.getElementById('question')
 const answers = document.getElementById('answers')
+const ans1 = document.getElementById('1')
+const ans2 = document.getElementById('2')
+const ans3 = document.getElementById('3')
+const ans4 = document.getElementById('4')
+const ansarr = [ans1, ans2, ans3, ans4]
 var randQuestion;
 var currentQuestionIndex;
-
 
 /**
  * Function that starts the game when user 
@@ -27,8 +31,14 @@ function newQuestion() {
 
 function showQuestion(question) {
   aquestion.innerText = question.question
-  answers.innerText = question.choices[0]
-  console.log()
+  ans1.innerText = question.choice1
+  ans2.innerText = question.choice2
+  ans3.innerText = question.choice3
+  ans4.innerText = question.choice4
 
+  ansarr.classList.add('hide')
 }
 
+function selectAnswer(params) {
+  
+}
